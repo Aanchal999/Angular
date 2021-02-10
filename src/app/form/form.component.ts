bc7cb821442b6dd5451abf4d0e6f1b9b0d879930
Aanchal Upadhayay <uaanchal551999@gmail.com>
2021-02-10 13:15:10 +0530

    event binding

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public Disable = true;
+  public name = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+  onClick()
+  {
+     alert("Fields Will be Enabled!!");
+     this.Disable=false;
+  }
+  
 
 }
